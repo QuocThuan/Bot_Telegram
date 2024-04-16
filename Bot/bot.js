72820b26a153034c0ae8454a5a53e997719c2db9
CherrylCao <caotranhonganh251299@gmail.com>
2024-04-16 11:22:02 +0700

    .env

---
+++ Bot/bot.js
@@ -0,0 +1,129 @@
+// const { Telegraf } = require("telegraf");
+// const TelegramBot = require('node-telegram-bot-api');
+// const token = "6465312406:AAG9bJ89G1IyRV8gZhRpoFNamd8oVcFnAYg";
+// // const bot = new Telegraf(TOKEN);
+// const bot = new TelegramBot(token, { polling: true });
+
+// // const web_link = "https://meek-meringue-1c4bf1.netlify.app";
+
+// bot.start((ctx) =>
+//     ctx.reply("Welcome :)))))", {
+//         reply_markup: {
+//             keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//         },
+//     })
+// );
+
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+//     console.log(msg)
+//     // send a message to the chat acknowledging receipt of their message
+//     bot.sendMessage(chatId, 'Received your message');
+// });
+
+// // bot.launch();
+
+// const http = require('http');
+// const { Telegraf } = require('telegraf');
+// const express = require('express');
+
+// const bot = new Telegraf('6465312406:AAG9bJ89G1IyRV8gZhRpoFNamd8oVcFnAYg');
+// const web_link = "https://meek-meringue-1c4bf1.netlify.app";
+// const port = process.env.PORT
+// const app = express();
+
+
+// bot.start((ctx) =>
+//     ctx.reply("Welcome :)))))", {
+//         reply_markup: {
+//             keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//         },
+//     })
+// );
+
+// // Xử lý tin nhắn từ người dùng
+// bot.on('text', (ctx) => {
+//     console.log('Received message:', ctx.message.text);
+//     // Xử lý tin nhắn ở đây và trả lời lại nếu cần
+// });
+
+// bot.launch();
+
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'POST' && req.url === '/increment') {
+//         let data = '';
+
+//         // Nghe sự kiện 'data' để nhận dữ liệu gửi lên từ phía client
+//         req.on('data', chunk => {
+//             data += chunk.toString(); // Chuyển đổi dữ liệu từ buffer thành chuỗi và cộng vào biến data
+//         });
+
+//         // Nghe sự kiện 'end' để biết khi dữ liệu kết thúc gửi lên
+//         req.on('end', () => {
+//             console.log('Received data:', data);
+
+//             bot.telegram.sendMessage('7003593765', `Total price: ${data} $`);
+
+//             // Phản hồi về cho client
+//             res.writeHead(200, { 'Content-Type': 'text/plain' });
+//             res.end('Data received');
+//         });
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         res.end('Not found');
+//     }
+// });
+
+// server.listen(port, () => {
+//     console.log('Web server is listening on port 3000');
+// });
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Telegraf } = require('telegraf');
+
+const app = express();
+const port = process.env.PORT;
+const bot = new Telegraf('6465312406:AAG9bJ89G1IyRV8gZhRpoFNamd8oVcFnAYg');
+const web_link = "https://meek-meringue-1c4bf1.netlify.app";
+console.log(port)
+// Sử dụng bodyParser để phân tích dữ liệu từ yêu cầu POST
+
+bot.start((ctx) =>
+    ctx.reply("Welcome :)))))", {
+        reply_markup: {
+            keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+        },
+    })
+);
+
+// Định nghĩa endpoint POST '/increment'
+app.post('/increment', (req, res) => {
+
+    let data = ''
+
+    req.on('data', chunk => {
+        data += chunk.toString();
+    })
+
+    req.on('end', () => {
+        console.log('Received data:', data);
+
+        bot.telegram.sendMessage('7003593765', `Total price: ${data} $`);
+
+        // Phản hồi về cho trang web
+        res.status(200).send('Data received');
+    })
+
+
+});
+
+app.use(bodyParser.json());
+
+bot.launch();
+
+// Lắng nghe yêu cầu trên cổng được cung cấp hoặc cổng mặc định 3000
+app.listen(port, () => {
+    console.log(`Web server is listening on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
-import BaiTapGlassesComponent from "./BaiTapGlasses/BaiTapGlassesComponent";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./Components/Card/Card";
+import Cart from "./Components/Cart/Cart";
+const { getData } = require("./db/db");
+const foods = getData();
+
+const tele = window.Telegram.WebApp;
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    tele.ready();
+  });
+
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
+    }
+  };
+
+  const onRemove = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
+    }
+  };
+
+  const onCheckout = (data) => {
+    console.log(data)
+    fetch('/increment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((res) => console.log(res.json()));
+    tele.MainButton.text = "Pay :)";
+    tele.MainButton.show();
+  };
+
+  // const clickCount = () => {
+  //   fetch('/increment', { method: 'POST' });
+  // }
+
   return (
-    <div
-      className="App"
-      style={{
-        backgroundImage: "url(./glassesImage/background.jpg)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "100vh",
-      }}
-    >
-      <BaiTapGlassesComponent />
-    </div>
+    <>
+      <h1 className="heading">Order Food</h1>
+      {/* <button onClick={() => { clickCount() }}>Count</button> */}
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
+      <div className="cards__container">
+        {foods.map((food) => {
+          return (
+            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
